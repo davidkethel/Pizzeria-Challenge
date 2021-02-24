6e2d4a282e77381faf5f14b75ce3da8ed3877fa7
DavidKethel <david.kethel@zuuse.com>
2021-02-25 07:40:08 +1000

    Added some new pocos

---
+++ LOR.Pizzeria/BakeingInstructions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LOR.Pizzeria
+{
+    public class BakeingInstructions
+    {
+        public int Time { get; set; }
+
+        public int Temperature { get; set; }
+    }
+}
