e3ae4cf389c05bfb597193509f9ec951ed796f15
DavidKethel <david.kethel@zuuse.com>
2021-02-25 20:31:26 +1000

    Added command classes

---
+++ LOR.Pizzeria/MakePizzaCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOR.Pizzeria
+{
+
+    class MakePizzaCommand : Command
+    {
+        private Kitchen Kitchen;
+
+        private MenuItem MenuItem;
+
+        public MakePizzaCommand(Kitchen kitchen, MenuItem menuItem)
+        {
+            Kitchen = kitchen;
+            MenuItem = menuItem;
+        }
+
+        public override Pizza Execute()
+        {
+            return Kitchen.PreparePizza(MenuItem);
+        }
+    }
+}
